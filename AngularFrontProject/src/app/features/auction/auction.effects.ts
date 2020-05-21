d17a37c8b5dd09cb838f277adc745610ac3114a1
Dmitry Veko <vekodmitriy0@gmail.com>
2020-05-21 15:43:35 +0300

    app/features/autcion/feat: add auction effects

--- AngularFrontProject/src/app/features/auction/auction.effects.ts
+++ AngularFrontProject/src/app/features/auction/auction.effects.ts
@@ -13,27 +13,27 @@ import {catchError, map, switchMap} from 'rxjs/operators';
 import {Auction} from '../../_interfaces/auction.interface';
 import {of} from 'rxjs';
 
-// Класс эффектов
+
 @Injectable()
 export class AuctionEffects {
   constructor(
-    private actions$: Actions, // данная переменная представляет из себя поток (observable), в который приходят все экшены, которые появляются в приложении
+    private actions$: Actions, 
     private auctionService: AuctionService,
     ) {
   }
 
-  // собственно сам эффект
+
   @Effect()
-    getAuctions$ = this.actions$.pipe( // перебираем все экшены, которые появляются в приложении
-      ofType(ActionTypes.GET_AUCTION_LIST), // проверям тип текущего экшена
-      switchMap(() => this.auctionService.getAuctions()), // вызываем метод сервиса и ретурним из switchMap результат работы сервиса (observable)
-      map((auctions: Auction[]) => new GetAuctionListSuccessAction(auctions)), // мап обернёт экшен в observable и он будет задиспатчен в стор (положим корректнрые данные в стор)
-      catchError((err) => of(new GetAuctionListFailureAction())) // отлавливаем ошибку
+    getAuctions$ = this.actions$.pipe( 
+      ofType(ActionTypes.GET_AUCTION_LIST), 
+      switchMap(() => this.auctionService.getAuctions()),
+      map((auctions: Auction[]) => new GetAuctionListSuccessAction(auctions)), 
+      catchError((err) => of(new GetAuctionListFailureAction())) 
     );
 
   @Effect()
   createAuction$ = this.actions$.pipe(
-    ofType<CreateAuctionAction>(ActionTypes.CREATE_AUCTION), // ofType возвращает дальше в цепочку экшен
+    ofType<CreateAuctionAction>(ActionTypes.CREATE_AUCTION), 
     switchMap((action: CreateAuctionAction) => this.auctionService.createAuction(action.payload)),
     map((auction: Auction) => new CreateAuctionSuccessAction(auction)),
     catchError((err) => of(new CreateAuctionFailureAction(err)))
