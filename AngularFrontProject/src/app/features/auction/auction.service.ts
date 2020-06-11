31fdb1fd1858836b73a568b823f6d1c7d7d7deb5
Dmitry Veko <vekodmitriy0@gmail.com>
2020-06-11 13:53:54 +0300

    feat: complete request to server on home page

--- AngularFrontProject/src/app/features/auction/auction.service.ts
+++ AngularFrontProject/src/app/features/auction/auction.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Observable, of, observable} from 'rxjs';
 import {Auction} from '../../_interfaces/auction.interface';
 import {AUCTION_MOCK_LIST} from '../../_helpers/mocks/auction-list.mock';
 import * as R from 'ramda';
 import {HttpClient} from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class AuctionService {
@@ -12,6 +13,7 @@ export class AuctionService {
   constructor(private readonly httpClient: HttpClient) {
   }
   private mockAuctionList: Auction[] = AUCTION_MOCK_LIST;
+  private auctions: Auction[] = [];
 
   createAuction(text: string): Observable<Auction> {
     const auction: Auction = {
@@ -24,7 +26,7 @@ export class AuctionService {
   }
 
   getAuctions(): Observable<Auction[]> {
-    return of(this.mockAuctionList);
+    return this.httpClient.get<Auction[]>(`${environment.apiUrl}/auction/getAll`);
   }
 
   toggleAuction(id: number): Observable<Auction> {
